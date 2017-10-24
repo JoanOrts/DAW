4e34464d9a200f584bd4b83f717ae7f5114d5f6d
xals1997 <xals1997@gmail.com>
2017-10-24 23:39:56 +0200

    Sacar algunas cosas comunes mas y cambiar include

--- Práctica 6/html/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-  <p>Pictures &amp; Images es una web creada por Joan Orts y Carlos Aracil para la asignatura de Desarrollo de Aplicaciones Web de la Universidad de Alicante</p>
-  <p>Copyright 2017</p>
-</footer>
--- Práctica 6/html/headerRegistrado.php
+++
@@ -1,8 +0,0 @@
-<header>
-  <div class="cabeceraizquierda"><a href="index.php"><img src="../images/logo.png" alt="Logo" width=170></a></div>
-  <div class="cabeceraderecha">
-    <a href="usuarioRegistrado.php">Jooann7</a>
-    <a href="usuarioRegistrado.php"><img src="../images/usuario.png" alt="icono usuario" width=40></a><br><br>
-    <nav class="enlacescabecera"><a href="busqueda.php"><em>Búsqueda avanzada</em></a></nav>
-  </div>
-</header>
