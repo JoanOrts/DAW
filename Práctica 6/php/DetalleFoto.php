7bdaa6abc4acf7a978de24c8de4f1b6be742cd14
xals1997 <xals1997@gmail.com>
2017-10-27 09:44:26 +0200

    practica 6

--- Práctica 6/php/DetalleFoto.php
+++ Práctica 6/php/DetalleFoto.php
@@ -16,7 +16,7 @@ require_once("header.php");
 				    <li>Fecha: 07/09/2017</li>
 				    <li>País: España</li>
 				    <li>Álbum: Viaje a Madrid</li>
-				    <li>Usuario: <a href="usuarioRegistrado.php">Joaan7</a></li>
+				    <li>Usuario: <a href="usuarioRegistrado.php?user=sergio7">sergio7</a></li>
 				</ul>
 			</figure>
 			<?php
@@ -30,7 +30,7 @@ require_once("header.php");
 				    <li>Fecha: 21/08/2012</li>
 				    <li>País: Finlandia</li>
 				    <li>Álbum: El fantástico Tobi</li>
-				    <li>Usuario: <a href="usuarioRegistrado.php">Joaan7</a></li>
+				    <li>Usuario: <a href="usuarioRegistrado.php?user=joan7">joan7</a></li>
 				</ul>
 			</figure>
 			<?php
