92c238cf5ff7c823b7ffbc712c41d27a6e9fbf8b
JoanOrts <jooann.2@hotmail.com>
2017-11-03 09:25:25 +0100

    id resultadobusqueda

--- Práctica 7/php/resultadosBusqueda.php
+++ Práctica 7/php/resultadosBusqueda.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 require_once("controlAcceso.php");
 $title="Resultados búsqueda. Pictures &amp; Images";
@@ -9,11 +9,11 @@ if(isset($_SESSION["Estado"])&&$_SESSION["Estado"]=="Autenticado"){
 else{
 	require_once("header.php");
 }
-?>	
+?>
 		<main>
 			<h2>Resultados de la búsqueda</h2>
 			<figure class="fotografia">
-                <a href="DetalleFoto.php"><img src="../images/foto3.jpg" alt="Imagen" width=400></a>
+                <a href="DetalleFoto.php?id=3"><img src="../images/foto3.jpg" alt="Imagen" width=400></a>
                 <ul>
                     <li>Título: <?php echo $_POST["titulo"];?></li>
                     <li>Fecha: <?php echo $_POST["dia"];?>/<?php echo $_POST["mes"];?>/<?php echo $_POST["año"];?></li>
@@ -21,7 +21,7 @@ else{
                 </ul>
 			</figure><br>
 			<figure class="fotografia">
-                <a href="DetalleFoto.php"><img src="../images/foto1.jpg" alt="Imagen" width=400></a>
+                <a href="DetalleFoto.php?id=1"><img src="../images/foto1.jpg" alt="Imagen" width=400></a>
                 <ul>
                     <li>Título: <?php echo $_POST["titulo"];?></li>
                     <li>Fecha: <?php echo $_POST["dia"];?>/<?php echo $_POST["mes"];?>/<?php echo $_POST["año"];?></li>
@@ -29,7 +29,7 @@ else{
                 </ul>
 			</figure><br>
 			<figure class="fotografia">
-                <a href="DetalleFoto.php"><img src="../images/foto2.jpg" alt="Imagen" width=400></a>
+                <a href="DetalleFoto.php?id=2"><img src="../images/foto2.jpg" alt="Imagen" width=400></a>
                 <ul>
                     <li>Título: <?php echo $_POST["titulo"];?></li>
                     <li>Fecha: <?php echo $_POST["dia"];?>/<?php echo $_POST["mes"];?>/<?php echo $_POST["año"];?></li>
